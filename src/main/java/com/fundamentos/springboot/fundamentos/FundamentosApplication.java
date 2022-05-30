11c02d5f0f52565fb0358a24208b14e4dd13ae1a
JesusRed <jesusalfredorv@ufps.edu.co>
2022-05-30 16:43:01 -0500

    pojos

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -2,7 +2,9 @@ package com.fundamentos.springboot.fundamentos;
 
 import com.fundamentos.springboot.fundamentos.bean.MyBean;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
+import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,13 +15,16 @@ public class FundamentosApplication implements CommandLineRunner {
 
     private ComponentDependency componentDependency;
     private MyBean myBean;
-
     private MyBeanWithDependency myBeanWithDependency;
+    private MyBeanWithProperties myBeanWithProperties;
+    private UserPojo userPojo;
 
-    public FundamentosApplication(@Qualifier("componentToImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency) {
+    public FundamentosApplication(@Qualifier("componentToImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo) {
         this.componentDependency = componentDependency;
         this.myBean = myBean;
         this.myBeanWithDependency = myBeanWithDependency;
+        this.myBeanWithProperties = myBeanWithProperties;
+        this.userPojo = userPojo;
     }
 
     public static void main(String[] args) {
@@ -31,5 +36,7 @@ public class FundamentosApplication implements CommandLineRunner {
         componentDependency.greetings();
         myBean.print();
         myBeanWithDependency.printWithDependency();
+        System.out.println(myBeanWithProperties.function());
+        System.out.println(userPojo.getEmail()+"/"+userPojo.getPassword());
     }
 }
