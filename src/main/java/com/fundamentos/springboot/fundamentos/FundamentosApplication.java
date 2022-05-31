e7730884f685f48920ad45c53784cc6f8cdf4279
JesusRed <jesusalfredorv@ufps.edu.co>
2022-05-31 17:21:47 -0500

    finish project

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -7,8 +7,10 @@ import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
 import com.fundamentos.springboot.fundamentos.entity.User;
 import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
 import com.fundamentos.springboot.fundamentos.repository.UserRepository;
+import com.fundamentos.springboot.fundamentos.service.UserService;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
+import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -30,14 +32,16 @@ public class FundamentosApplication implements CommandLineRunner {
     private MyBeanWithProperties myBeanWithProperties;
     private UserPojo userPojo;
     private UserRepository userRepository;
+    private UserService userService;
 
-    public FundamentosApplication(@Qualifier("componentToImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo, UserRepository userRepository) {
+    public FundamentosApplication(@Qualifier("componentToImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo, UserRepository userRepository, UserService userService) {
         this.componentDependency = componentDependency;
         this.myBean = myBean;
         this.myBeanWithDependency = myBeanWithDependency;
         this.myBeanWithProperties = myBeanWithProperties;
         this.userPojo = userPojo;
         this.userRepository = userRepository;
+        this.userService = userService;
     }
 
     public static void main(String[] args) {
@@ -49,15 +53,63 @@ public class FundamentosApplication implements CommandLineRunner {
         //beforeClasses();
         saveUsersInDatabase();
         getInfoJpqlUser();
+        saveErrorTransac();
     }
 
-    public void getInfoJpqlUser(){
+    public void getInfoJpqlUser() {
         LOGGER.info("User by findbyemail: " + userRepository.findByUserEmail("juan@mail.com").
-                orElseThrow(()-> new RuntimeException("user not exists")));
+                orElseThrow(() -> new RuntimeException("user not exists")));
 
         userRepository.findAndSort("J", Sort.by("id").descending())
                 .stream()
-                .forEach(user -> LOGGER.info("UserList with Sort method " +user));
+                .forEach(user -> LOGGER.info("UserList with Sort method " + user));
+
+        userRepository.findByName("Wan")
+                .stream()
+                .forEach(user -> LOGGER.info("User by query method " + user));
+
+        LOGGER.info("Usuario by queryMethod findByEmailAndName" + userRepository.findByEmailAndName("oan@mail.com", "Oan")
+                .orElseThrow(() -> new RuntimeException("User not exists")));
+
+        userRepository.findByNameLike("%ua%")
+                .stream()
+                .forEach(user -> LOGGER.info("User findByNameLike: " + user));
+
+        userRepository.findByNameOrEmail(null, "juan@mail.com")
+                .stream()
+                .forEach(user -> LOGGER.info("User findByNameOrEmail: " + user.getName()));
+
+        userRepository.findByBirthDateBetween(
+                        LocalDate.of(1996, 1, 1), LocalDate.of(1997, 12, 31))
+                .stream()
+                .forEach(user -> LOGGER.info("User by findByBirthDateBetween: " + user.getName()));
+
+        userRepository.findByNameLikeOrderByIdDesc("%an%")
+                .stream()
+                .forEach(user -> LOGGER.info("User by findByNameLikeOrderByIdDesc: " + user.getName() + "-" + user.getId()));
+
+        userRepository.findByNameContainingOrderByIdAsc(("u"))
+                .stream()
+                .forEach(user -> LOGGER.info("User by findByNameContainingOrderByIdAsc: " + user));
+
+        LOGGER.info("User form named parameter is: " + userRepository.getAllByBirthDateAndEmail(LocalDate.of(1995, 8, 13), "jooan@mail.com")
+                .orElseThrow(() -> new RuntimeException("User not exits getAllByBirthDateAndEmail")));
+    }
+
+    private void saveErrorTransac() {
+        User test1 = new User("TestTransactional1", "test1@mail.com", LocalDate.now());
+        User test2 = new User("TestTransactional2", "test2@mail.com", LocalDate.now());
+        User test3 = new User("TestTransactional3", "test5@mail.com", LocalDate.now());
+        User test4 = new User("TestTransactional4", "test4@mail.com", LocalDate.now());
+        List<User> users = Arrays.asList(test1, test2, test3, test4);
+        try {
+            userService.saveTransactional(users);
+        } catch (Exception e) {
+            LOGGER.error("Transactional error" + e.toString());
+        }
+
+        userService.getAllUsers().stream()
+                .forEach(user -> LOGGER.info("This usere is by Transactional " + user));
     }
 
     private void saveUsersInDatabase() {
@@ -68,6 +120,7 @@ public class FundamentosApplication implements CommandLineRunner {
         User user5 = new User("Jooan", "jooan@mail.com", LocalDate.of(1995, 8, 13));
         List<User> list = Arrays.asList(user1, user2, user3, user4, user5);
         userRepository.saveAll(list);
+
     }
 
     private void beforeClasses() {
--- src/main/java/com/fundamentos/springboot/fundamentos/repository/UserRepository.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/repository/UserRepository.java
@@ -1,20 +1,45 @@
 package com.fundamentos.springboot.fundamentos.repository;
 
+import com.fundamentos.springboot.fundamentos.dto.UserDTO;
 import com.fundamentos.springboot.fundamentos.entity.User;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.email=?1")
     Optional<User> findByUserEmail(String email);
 
     @Query("SELECT u FROM User u WHERE u.name LIKE ?1%")
     List<User> findAndSort(String name, Sort sort);
 
+    List<User> findByName(String name);
+
+    Optional<User> findByEmailAndName(String email, String name);
+
+    List<User> findByNameLike(String name);
+
+    List<User> findByNameOrEmail(String name, String email);
+
+    List<User> findByBirthDateBetween(LocalDate begin, LocalDate end);
+
+    List<User> findByNameLikeOrderByIdDesc(String name);
+
+    List<User> findByNameContainingOrderByIdAsc(String name);
+
+    @Query("SELECT NEW com.fundamentos.springboot.fundamentos.dto.UserDTO(u.id, u.name, u.birthDate)"+
+            " FROM User u " +
+            " WHERE u.birthDate=:parameterDate " +
+            " AND u.email=:parameterEmail ")
+    Optional<UserDTO> getAllByBirthDateAndEmail(@Param("parameterDate") LocalDate birthdate, @Param("parameterEmail") String email);
+
+    List<User> findAll();
 }
